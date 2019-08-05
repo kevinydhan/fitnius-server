c1cab5df394a84c6a472ab271656f0b0e5a9506c
Kevin Han <yd.han181@gmail.com>
2019-08-04 20:26:40 -0400

    Added conditional database credentials to Sequelize connection.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,18 +1,29 @@
 import { Sequelize } from 'sequelize-typescript'
 
-console.log()
-console.log('=================================================================')
-console.log('NODE_ENV =', process.env.NODE_ENV)
-console.log('DATABASE_URL =', process.env.DATABASE_URL)
-console.log('=================================================================')
-console.log()
+interface DatabaseCredentials {
+    username: string | undefined
+    password: string | undefined
+    database: string | undefined
+}
+
+const credentials: DatabaseCredentials = {
+    username: '',
+    password: '',
+    database: 'fitnus-db'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    ;(credentials.username = process.env.DB_USER),
+        (credentials.password = process.env.DB_PASSWORD),
+        (credentials.database = process.env.DATABASE_URL)
+}
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    database: process.env.DATABASE_URL || 'fitnius-db',
     storage: ':memory:',
     models: [__dirname + '/models', __dirname + '/models' + '/joins'],
-    logging: false
+    logging: false,
+    ...credentials
 })
 
 export default sequelize

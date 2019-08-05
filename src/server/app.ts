6a1603f7842e52c25f9a3e30ea74a424d3dbc41a
Kevin Han <yd.han181@gmail.com>
2019-08-05 12:00:02 -0400

    Added GraphQL HTTP into Express app. Tested query for MuscleGroup.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,14 +1,63 @@
 import express from 'express'
-
-const app = express()
+import graphqlHttp from 'express-graphql'
+import { buildSchema } from 'graphql'
 
 import { Exercise } from '../database/models/Exercise'
 import { MuscleGroup } from '../database/models/MuscleGroup'
 import { Equipment } from '../database/models/Equipment'
 
+const app = express()
+
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
+app.use(
+    '/graphql',
+    graphqlHttp({
+        graphiql: true,
+        schema: buildSchema(`
+        type Exercise {
+            id: ID!
+            name: String!
+            level: Int!
+            rating: Int
+            createdAt: String
+            updatedAt: String
+        }
+
+        type MuscleGroup {
+            id: ID!
+            name: String!
+            exercises: [Exercise!]!
+            createdAt: String
+            updatedAt: String
+        }
+
+        input MuscleGroupInput {
+            name: String!
+        }
+
+        type RootQuery {
+            muscleGroups: [MuscleGroup!]!
+        }
+
+        schema {
+            query: RootQuery
+        }
+        `),
+        rootValue: {
+            muscleGroups: async () => {
+                try {
+                    const muscleGroups = await MuscleGroup.findAll()
+                    return muscleGroups
+                } catch (err) {
+                    throw err
+                }
+            }
+        }
+    })
+)
+
+app.get('/', (req: express.Request, res: express.Response, next) => {
     res.send('hello')
 })
 

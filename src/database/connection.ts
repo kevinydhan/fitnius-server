35cd00df5a0ffc120c67918d491c53b4d89a0972
Kevin Han <yd.han181@gmail.com>
2019-08-04 21:04:20 -0400

    Removed database credentials

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,35 +1,17 @@
 import { Sequelize } from 'sequelize-typescript'
 
-// interface DatabaseCredentials {
-//     username: string | undefined
-//     password: string | undefined
-//     database: string | undefined
-// }
+const ssl = process.env.NODE_ENV === 'production'
 
-// const credentials: DatabaseCredentials = {
-//     username: '',
-//     password: '',
-//     database: 'fitnius-db'
-// }
-
-// if (process.env.NODE_ENV === 'production') {
-//     ;(credentials.username = process.env.DB_USER),
-//         (credentials.password = process.env.DB_PASSWORD),
-//         (credentials.database = process.env.DATABASE_URL)
-// }
+console.log('pls work')
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
     protocol: 'postgres',
-    username: process.env.DB_USER || '',
-    password: process.env.DB_PASSWORD || '',
     database: process.env.DATABASE_URL || 'fitnius-db',
     storage: ':memory:',
-    models: [__dirname + '/models', __dirname + '/models' + '/joins'],
     logging: false,
-    dialectOptions: {
-        ssl: true
-    }
+    dialectOptions: { ssl },
+    models: [__dirname + '/models', __dirname + '/models' + '/joins']
 })
 
 export default sequelize

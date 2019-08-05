6a98c2d7513b81dfcf64225540ba4b67a31b77f7
Kevin Han <yd.han181@gmail.com>
2019-08-05 00:40:06 -0400

    Added DB_HOST environment variable

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,9 +2,14 @@ import { Sequelize } from 'sequelize-typescript'
 
 const ssl = process.env.NODE_ENV === 'production'
 
+console.log('DB_HOST =', process.env.DB_HOST)
+console.log('DB_USER =', process.env.DB_USER)
+console.log('DB_PASSWORD =', process.env.DB_PASSWORD)
+
 export const connection = new Sequelize({
     dialect: 'postgres',
     protocol: 'postgres',
+    host: process.env.DB_HOST || '',
     username: process.env.DB_USER || '',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DATABASE_URL || 'fitnius-db',

b6c8cb620970adf5397e82f3165a308e549c34b2
Kevin Han <yd.han181@gmail.com>
2019-08-04 20:02:10 -0400

    Debugging Heroku crash.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from 'sequelize-typescript'
 
+console.log()
+console.log('=================================================================')
+console.log('NODE_ENV =', process.env.NODE_ENV)
 console.log('DATABASE_URL =', process.env.DATABASE_URL)
+console.log('=================================================================')
+console.log()
 
 const sequelize = new Sequelize({
     dialect: 'postgres',

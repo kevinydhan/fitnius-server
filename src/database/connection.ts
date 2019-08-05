d30b95ea41842600cace82019794608ef7e095da
Kevin Han <yd.han181@gmail.com>
2019-08-05 00:33:31 -0400

    Added username and password fields to Sequelize connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,6 +5,8 @@ const ssl = process.env.NODE_ENV === 'production'
 export const connection = new Sequelize({
     dialect: 'postgres',
     protocol: 'postgres',
+    username: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DATABASE_URL || 'fitnius-db',
     logging: false,
     dialectOptions: { ssl },

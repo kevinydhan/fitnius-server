01c088eb71bafbdbfeb8ba1c5bebad47c8099455
Kevin Han <yd.han181@gmail.com>
2019-08-04 20:52:40 -0400

    Modified Sequelize connection statement.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,34 +1,32 @@
 import { Sequelize } from 'sequelize-typescript'
 
-interface DatabaseCredentials {
-    username: string | undefined
-    password: string | undefined
-    database: string | undefined
-}
+// interface DatabaseCredentials {
+//     username: string | undefined
+//     password: string | undefined
+//     database: string | undefined
+// }
 
-const credentials: DatabaseCredentials = {
-    username: '',
-    password: '',
-    database: 'fitnius-db'
-}
+// const credentials: DatabaseCredentials = {
+//     username: '',
+//     password: '',
+//     database: 'fitnius-db'
+// }
 
-console.log()
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-console.log()
-
-if (process.env.NODE_ENV === 'production') {
-    ;(credentials.username = process.env.DB_USER),
-        (credentials.password = process.env.DB_PASSWORD),
-        (credentials.database = process.env.DATABASE_URL)
-}
+// if (process.env.NODE_ENV === 'production') {
+//     ;(credentials.username = process.env.DB_USER),
+//         (credentials.password = process.env.DB_PASSWORD),
+//         (credentials.database = process.env.DATABASE_URL)
+// }
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
+    protocol: 'postgres',
+    username: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DATABASE_URL || 'fitnius-db',
     storage: ':memory:',
     models: [__dirname + '/models', __dirname + '/models' + '/joins'],
-    logging: false,
-    ...credentials
+    logging: false
 })
 
 export default sequelize

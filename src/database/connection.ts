db2ccfb0a82e2428e5690154627f4b8b8f9f93de
Kevin Han <yd.han181@gmail.com>
2019-08-05 00:42:38 -0400

    Removed database credentials from Sequelize connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -9,9 +9,6 @@ console.log('DB_PASSWORD =', process.env.DB_PASSWORD)
 export const connection = new Sequelize({
     dialect: 'postgres',
     protocol: 'postgres',
-    host: process.env.DB_HOST || '',
-    username: process.env.DB_USER || '',
-    password: process.env.DB_PASSWORD || '',
     database: process.env.DATABASE_URL || 'fitnius-db',
     logging: false,
     dialectOptions: { ssl },

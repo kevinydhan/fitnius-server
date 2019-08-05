6930c018084bdf2e20fa2464dbfc27cde324879b
Kevin Han <yd.han181@gmail.com>
2019-08-04 23:44:35 -0400

    Removed node_modules from excludes in tsconfig.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,7 @@ import { connection } from '../database/connection'
 
 const PORT = process.env.PORT || 3000
 connection
-    .sync({ force: false })
+    .sync()
     .then(() => {
         app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
     })

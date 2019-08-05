2bfd44de028ec347dac2969580df91f3014dcad4
Kevin Han <yd.han181@gmail.com>
2019-08-04 21:17:41 -0400

    Removed storage key in Sequelize connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,11 +2,10 @@ import { Sequelize } from 'sequelize-typescript'
 
 const ssl = process.env.NODE_ENV === 'production'
 
-export const sequelize = new Sequelize({
+export const connection = new Sequelize({
     dialect: 'postgres',
     protocol: 'postgres',
     database: process.env.DATABASE_URL || 'fitnius-db',
-    storage: ':memory:',
     logging: false,
     dialectOptions: { ssl },
     models: [__dirname + '/models', __dirname + '/models' + '/joins']
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,8 @@
 import app from './app'
-import { sequelize } from '../database/connection'
+import { connection } from '../database/connection'
 
 const PORT = process.env.PORT || 3000
-sequelize
+connection
     .sync({ force: false })
     .then(() => {
         app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
